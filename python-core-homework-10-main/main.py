ae9bafd7ef64f9e559c112b2d9e412a1d8f08b73
lomakina0612 <lomakina0612@gmail.com>
2023-10-08 20:05:35 +0200

    initial commit

---
+++ python-core-homework-10-main/main.py
@@ -0,0 +1,96 @@
+from collections import UserDict
+import re
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Name(Field):
+    def __init__(self, value):
+        super().__init__(value)
+
+
+class Phone(Field):
+    def __init__(self, value):
+        if not self.is_valid_phone(value):
+            raise ValueError("Invalid phone number format")
+        super().__init__(value)
+
+    @staticmethod
+    def is_valid_phone(phone):
+        # Уточним регулярное выражение для номера телефона
+        pattern = r"^\+?\d{1,4}[\d\- ]*$"
+        if not re.match(pattern, phone):
+            return False
+
+        # Добавим проверку длины номера
+        if len(re.sub(r"[^0-9]", "", phone)) < 10:
+            return False
+
+        return True
+
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                self.phones.remove(p)
+                return True
+        return False
+
+    def edit_phone(self, old_phone, new_phone):
+        for p in self.phones:
+            if p.value == old_phone:
+                p.value = new_phone
+                return True
+        return False
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p
+        return None
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+    def edit_phone(self, old_phone, new_phone):
+        found = False
+        for phone in self.phones:
+            if phone.value == old_phone:
+                phone.value = new_phone
+                found = True
+                break
+
+        if not found:
+            raise ValueError(f"Phone number '{old_phone}' not found in the record")
+
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        if name in self.data:
+            return self.data[name]
+        else:
+            return None
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+            return True
+        else:
+            return False
